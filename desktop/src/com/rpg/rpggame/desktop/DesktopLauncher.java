76f56537ed5ae12e0e8b51d016ddaaa4c0815a06
Ash228 <52212601+Ash228@users.noreply.github.com>
2021-07-17 21:29:16 +0530

    Initial Commit Added project

---
+++ desktop/src/com/rpg/rpggame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rpg.rpggame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rpg.rpggame.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
